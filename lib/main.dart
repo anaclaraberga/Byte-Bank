db350efac126749da70393abef1b1d11c2170273
Ana Clara <ana4ew.br@gmail.com>
2024-07-29 19:42:03 -0300

    Updated: Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Text('Bem-vindo', textDirection: TextDirection.ltr));
+  runApp(const Column(
+    children: <Widget>[
+      Text(
+        'Deliver features faster',
+        textDirection: TextDirection.ltr,
+      ),
+      Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
+      Expanded(
+        child: FittedBox(
+          child: FlutterLogo(),
+        ),
+      ),
+    ],
+  ));
 }
