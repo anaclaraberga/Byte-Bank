dbf8dc7d99e65f52bd73ef0908197bf9946ce130
Ana Clara <ana4ew.br@gmail.com>
2024-07-30 13:36:04 -0300

    Update: Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,9 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: const Column(
-        children: [
-          Card(
-              child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.00'),
-            subtitle: Text('1000'),
-          )),
-          Card(
-              child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('140.00'),
-            subtitle: Text('1000'),
-          )),
-        ],
-      ),
+      body: TransferList(),
       appBar: AppBar(
         title: const Text('Transferências'),
       ),
@@ -32,4 +15,34 @@ void main() {
       ),
     ),
   ));
+}
+
+class TransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        TransferItem('100.00', 'Conta: 2944'),
+        TransferItem('50.00', 'Conta: 2964'),
+        TransferItem('160.00', 'Conta: 2454'),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final String value;
+  final String accountNumber;
+
+  const TransferItem(this.value, this.accountNumber);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(value),
+      subtitle: Text(accountNumber),
+    ));
+  }
 }
