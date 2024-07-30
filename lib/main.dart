d6c72f9679b9cab0d40685d6cb08bf274b52713e
Ana Clara <ana4ew.br@gmail.com>
2024-07-29 21:41:43 -0300

    Updated: Testing Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const Column(
@@ -8,6 +9,9 @@ void main() {
         textDirection: TextDirection.ltr,
       ),
       Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
+      Column(children: <Widget>[
+        Text('Make art as a you can', textDirection: TextDirection.ltr)
+      ]),
       Expanded(
         child: FittedBox(
           child: FlutterLogo(),
